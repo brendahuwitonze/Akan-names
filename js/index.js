4b88571ec011d488e1886f0a74a2e7d5e96aa153
uwitonze brendah <uwitonzebrendah@gmail.com>
2020-09-27 15:34:13 +0200

    added if and else statements

--- js/index.js
+++ js/index.js
@@ -1,41 +1,75 @@
-function names(){
-    var year=document.getElementById("year").value
-    var mm=document.getElementById("month").value
-    var dd=document.getElementById("date").value
-    var gen=document.getElementById("gender").value
-    
-    var arr = [];
-    while(year>0)
-    {
-        arr.unshift(year%10);
-        year=year/10|0;
-     }
-    console.log(arr);
-    
-    var y = 0;
-    for (var i = 3; i>1 ; i--)
-    {
-         y= 10 * y + arr[i];
-    }
-    console.log(y);
-    
-    
-    var yy= String(y).split("").reverse().join("");
-    console.log(yy); 
-    
-    
-    var cc = 0;
-    for (var i = 0; i<2 ; i++)
-    {
-        cc = 10 * cc + arr[i];
-        }
-        console.log(cc)
-        ;
-       var d = ( ( (cc/4) -2*cc-1) + ( (5*yy/4) ) + ((26*(mm+1)/10) ) + dd)%7+1;
-      var da= (Math.floor(d));
-        document.getElementById("display").innerHTML=yy
 
-        
-    }
+function birth() {
+    var myBirthday = document.getElementById("myBirthDate").value;
+    var myGender = document.getElementById("gender").value
+    var dateOfBirth = new Date(myBirthday);
+    var dayOfTheWeek = dateOfBirth.getDay();
+    var akan;
+    var day;
     
-    
+     if (myGender=="Female"){
+         if(dayOfTheWeek==1){
+             akan="Adwoa";
+             day="Monday"
+         }
+         else if (dayOfTheWeek==2){
+            akan="Abenaa"
+            day="Tuesday"
+         }
+         else if(dayOfTheWeek==3){
+             akan="Akua"
+             day="Wednesday"
+         }
+         else if(dayOfTheWeek==4){
+             akan="Yaa"
+             day="Thursday"
+         }
+         else if (dayOfTheWeek==5){
+             akan="Afua"
+             day="Friday"
+         }
+         else if(dayOfTheWeek==6){
+             akan="Ama"
+             day="Saturday"
+         }
+         else {
+             akan="Akosua"
+             day="Sunday"
+         }
+     }
+     else {
+        if(dayOfTheWeek==1){
+            akan="Kwadwo";
+            day="Monday"
+        }
+        else if (dayOfTheWeek==2){
+           akan="Kwabena"
+           day="Tuesday"
+        }
+        else if(dayOfTheWeek==3){
+            akan="Kwaku"
+            day="Wednesday"
+        }
+        else if(dayOfTheWeek==4){
+            akan="Yaw"
+            day="Thursday"
+        }
+        else if (dayOfTheWeek==5){
+            akan="Kofi"
+            day="Friday"
+        }
+        else if(dayOfTheWeek==6){
+            akan="Kwame"
+            day="Saturday"
+        }
+        else {
+            akan="Kwasi"
+            day="Sunday"
+        }
+       }
+    document.getElementById("result").innerHTML = day;
+
+
+     
+}
+
